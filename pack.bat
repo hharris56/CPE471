5ce1e831e13f2ecdc472629d3dc6baaa7ca71dd9
hharris-esaero <hunter.harris@esaero.com>
2019-12-02 11:02:20 -0800

    pack.bat working, github.bat now cleans and packs directories!

--- pack.bat
+++ pack.bat
@@ -2,6 +2,10 @@
 Title Packaging
 echo Zipping Directories
 
+REM for /d %%i in ("%cd%"\*) do (
 for /d %%i in ("%cd%"\*) do (
-    zip -r -p %%i
+    REM zip -r -p %%i
+    echo %%i
+    "%ProgramFiles%\WinRAR\Rar.exe" a -ep1 -idq -r -y %%i %%i
 )
+pause
